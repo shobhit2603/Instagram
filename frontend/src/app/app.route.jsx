b812d4544e7975a1e115cb21b4d238298f5f86b7
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-04-02 12:24:30 +0530

    Working on Search User feature

--- frontend/src/app/app.route.jsx
+++ frontend/src/app/app.route.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
 import Home from "../features/posts/pages/Home";
-import Profile from "../features/posts/pages/Profile";
+import Profile from "../features/users/pages/Profile";
 import MainLayout from "./MainLayout";
 
 const router = createBrowserRouter([
