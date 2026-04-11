87eea4b24b1bc4bb693ced1d802942c425b575e9
shobhit2603 <shrivastavashobhit706@gmail.com>
2026-04-11 12:33:01 +0530

    Added Aggregation in Chat Controller

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -1,29 +1,63 @@
 import followModel from "../models/follow.model.js";
-import User from "../models/user.model.js";
 
 export const getUsers = async (req, res) => {
   const loggedInUserId = req.user.id;
-  const user = await User.findById(loggedInUserId);
 
-  if (!user) {
-    return res.status(404).json({ message: "User not found", success: false })
-  }
-
-  // Get all users the current user follows or who follow the current user
-  const follows = await followModel.find({
-    $or: [{ followee: user.username }, { follower: user.username }],
-    status: "accepted",
-  });
-
-  const connectedUsernames = new Set();
-  follows.forEach(f => {
-    if (f.follower !== user.username) connectedUsernames.add(f.follower);
-    if (f.followee !== user.username) connectedUsernames.add(f.followee);
-  });
-
-  const users = await User.find({
-    username: { $in: Array.from(connectedUsernames) }
-  }).select("username profileImage fullName");
+  const users = await followModel.aggregate(
+    [
+      {
+        $match: {
+          $or: [{ followee: loggedInUserId }, { follower: loggedInUserId }],
+          status: "accepted",
+        },
+      },
+      {
+        $addFields: {
+          user: {
+            $cond: {
+              if: {
+                $eq: ["$follower", loggedInUserId],
+              },
+              then: "$followee",
+              else: "$follower",
+            },
+          },
+        },
+      },
+      {
+        $group: {
+          _id: "$user",
+        },
+      },
+      {
+        $addFields: {
+          userObjId: { $toObjectId: "$_id" },
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "userObjId",
+          foreignField: "_id",
+          as: "userDetails",
+        },
+      },
+      {
+        $unwind: {
+          path: "$userDetails",
+          preserveNullAndEmptyArrays: false,
+        },
+      },
+      {
+        $project: {
+          _id: "$userDetails._id",
+          username: "$userDetails.username",
+          profilePicture: "$userDetails.profileImage",
+        },
+      },
+    ],
+    { maxTimeMS: 60000, allowDiskUse: true },
+  );
 
   return res.status(200).json({
     message: "Users fetched successfully",
